70e6b3f22beb5dd9db670693ceb5c0e1094d2136
Ian Hornik <yiliansource@gmail.com>
2021-04-21 17:58:20 +0200

    Fixed build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
-gulp.task('sass', function() {
-    return gulp.src("app/sass/*.sass")
+gulp.task("sass", function () {
+    return gulp
+        .src("app/sass/*.sass")
         .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', gulp.series('sass', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./app"
-        }
-    });
+gulp.task(
+    "serve",
+    gulp.series("sass", function () {
+        browserSync.init({
+            server: {
+                baseDir: "./app",
+            },
+        });
 
-    gulp.watch("app/sass/*.sass", gulp.series('sass'));
-    gulp.watch("app/*html").on("change", browserSync.reload);
-}));
+        gulp.watch("app/sass/*.sass", gulp.series("sass"));
+        gulp.watch("app/*html").on("change", browserSync.reload);
+    })
+);
 
-gulp.task('default', gulp.series('serve'));
+gulp.task("build", gulp.series("serve"));
